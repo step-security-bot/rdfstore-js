6e8472575e86309a7ea4ff837c25a76d6a63de6e
musicEnfanthen <stefan.muennich@unibas.ch>
2023-05-09 17:22:25 +0200

    Version bump 0.9.18-alpha.12

--- lubm/browser/rdfstore.js
+++ lubm/browser/rdfstore.js
@@ -19924,7 +19924,7 @@ Store.prototype.close = function(cb) {
 /**
  * Version of the store
  */
-Store.VERSION = "0.9.18-alpha.11";
+Store.VERSION = "0.9.18-alpha.12";
 
 /**
  * Create a new RDFStore instance that will be
